0fb1c63197dbc46a38066c17ed07e64f0358d312
Diego Alvarez S. <dialvarezs@gmail.com>
2026-03-09 15:31:01 -0300

    chore: bump version

--- sylphy/__init__.py
+++ sylphy/__init__.py
@@ -12,7 +12,7 @@ Re-exports core registry/config symbols at the top-level so that users can:
     pr.get_config()
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # Public core API (re-export)
 from .core import (  # noqa: E402
